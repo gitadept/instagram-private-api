d43418588d99315ca3dfa0fa073547ddb69d8fbe
Eugene Bashmakov <septents@gmail.com>
2017-07-24 18:09:01 +0300

    Add cookie-redis-storage

---
+++ client/v1/cookie-redis-storage.js
@@ -0,0 +1,17 @@
+var util = require('util');
+
+var RedisCookieStore = require('redis-cookie-monster');
+var CookieStorage = require('./cookie-storage');
+
+function CookieRedisStorage(id, options) {
+  if (!options)
+    options = {
+      port: 6379,
+      host: '127.0.0.1'
+    };
+
+  CookieStorage.call(this, new RedisCookieStore(`instagramcookie:${id}`, options));
+}
+
+util.inherits(CookieRedisStorage, CookieStorage);
+module.exports = CookieRedisStorage;
