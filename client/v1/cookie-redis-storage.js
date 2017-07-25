993daa1c825eaaae44aa6e5588cd4d0cd973b518
Eugene Bashmakov <septents@gmail.com>
2017-07-25 10:59:02 +0300

    Add support for getAllCookies

--- client/v1/cookie-redis-storage.js
+++ client/v1/cookie-redis-storage.js
@@ -2,15 +2,28 @@ var util = require('util');
 
 var RedisCookieStore = require('redis-cookie-monster');
 var CookieStorage = require('./cookie-storage');
+var CONSTANTS = require("./constants");
 
 function CookieRedisStorage(id, options) {
-  if (!options)
+  if (!options) {
     options = {
       port: 6379,
       host: '127.0.0.1'
     };
+  }
 
-  CookieStorage.call(this, new RedisCookieStore(`instagramcookie:${id}`, options));
+  const store = new RedisCookieStore(`instagramcookie:${id}`, options);
+
+  store.__proto__.getAllCookies = function(cb) {
+    store.findCookies(CONSTANTS.HOSTNAME, '/', function(err, cookies) {
+      cookies.sort(function(a, b) {
+        return (a.creationIndex || 0) - (b.creationIndex || 0);
+      });
+      cb(null, cookies);
+    });
+  };
+
+  CookieStorage.call(this, store);
 }
 
 util.inherits(CookieRedisStorage, CookieStorage);
